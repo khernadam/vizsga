b2599ae41b6cc422754c50d510330992cbd5b6d4
khernadam <khern.adam@gmail.com>
2021-04-08 15:52:55 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,6 +9,8 @@
             <p>These are my contacts:</p>
             <p>Phone: +36203842529</p>
             <p>E-mail: <a href="mailto:khern.adam@gmail.com">khern.adam@gmail.com</a></p>
+            <p>Google maps:</p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2769.798706014097!2d18.214576050537172!3d46.035164903082475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4742b3b64547f9ff%3A0x81835de2edb802f1!2sSimonyi%20K%C3%A1roly%20Vocational%20and%20Technical%20School!5e0!3m2!1sen!2shu!4v1616346508128!5m2!1sen!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
 <?php
